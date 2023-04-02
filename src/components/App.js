4f332de33e5288e0dc26f6bb878fa7510132a49c
Huseni Sameer Jaker <103500196+sameerhuseni@users.noreply.github.com>
2023-04-02 13:13:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,33 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addAppointment } from '../action';
-import '../styles/App.css';
+import { addAppointment } from './action';
 
 const App = () => {
   const [name, setName] = useState('');
   const [time, setTime] = useState('');
   const [description, setDescription] = useState('');
+  const appointments = useSelector(state => state.appointments);
+  const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
+    dispatch(addAppointment(name, time, description));
+    setName('');
+    setTime('');
+    setDescription('');
   };
 
   return (
-    <div id="main">
+    <div>
       <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input type="text" className="name-input"/>
-        <br />
-        <label>Time:</label>
-        <input type="text" className="time-input"/>
-        <br />
-        <label>Description:</label>
-        <textarea className="description-input"/>
-        <br />
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
+        <label htmlFor="time">Time:</label>
+        <input type="text" id="time" value={time} onChange={e => setTime(e.target.value)} />
+        <label htmlFor="description">Description:</label>
+        <input type="text" id="description" value={description} onChange={e => setDescription(e.target.value)} />
         <button type="submit">Add Appointment</button>
       </form>
-      <br />
       <table>
         <thead>
           <tr>
@@ -36,17 +37,10 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-            {/* Appointment HTML */}
-            <tr>
-              <td>appointmentName</td>
-              <td>appointmentTime</td>
-              <td>appointmentDescription</td>
+          {appointments.map((appointment, index) => (
+            <tr key={index}>
+              <td>{appointment.name}</td>
+              <td>{appointment.time}</td>
+              <td>{appointment.description}</td>
             </tr>
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
-
-export default App;
+          ))}
