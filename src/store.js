1d57dd9e6da710fbb0700c2cd331ba0ab3da956e
Huseni Sameer Jaker <103500196+sameerhuseni@users.noreply.github.com>
2023-04-02 13:11:31 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,18 +1,21 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  appointments: [],
+  appointments: []
 };
 
-function appointmentReducer(state = initialState, action) {
+const appointmentReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_APPOINTMENT':
-      return {};
+      return {
+        ...state,
+        appointments: [...state.appointments, action.payload]
+      };
     default:
       return state;
   }
-}
+};
 
-const store = createStore();
+const store = createStore(appointmentReducer);
 
 export default store;
